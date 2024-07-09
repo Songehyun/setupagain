bce461d9e731d7da54319cbc6b7b15d5c79b7044
송이현 <songehyun00@gmail.com>
2024-07-09 12:01:37 +0900

    sav_송이현 : 중간저장

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,19 @@
 import React from "react";
+import minam from "../static/basic-data";
 
 interface textNode {
   textNode: string;
 }
 
 const Minam: React.FC<textNode> = ({ textNode }) => {
-  // 인터페이스에 <>로 무언가 추가되는게 제네릭 문법
   return <h1>{textNode}</h1>;
 };
 
 const App: React.FC = () => {
   return (
     <div>
-      <Minam textNode="공욱재" />
-      <h1>안녕</h1>
+      <Minam textNode={minam} />
+      <h1>안녕?</h1>
     </div>
   );
 };
