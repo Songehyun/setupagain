59ed8503174e1dd9de5963a56e0e4d6721542dc3
송이현 <songehyun00@gmail.com>
2024-07-09 11:45:58 +0900

    edit_송이현 : index.tsx 파일 수정

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Minam = () => {
+  return <h1>공욱재</h1>;
+};
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Minam />
+      return <h1>안녕</h1>;
+    </div>
+  );
+};
+
+export default App;
